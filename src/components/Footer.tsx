4415ce64075a4a2869205e9c2134364c9ab0711e
mrlzzz <morelzzzz@gmail.com>
2024-01-17 00:49:43 +0100

    fix: footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mx-auto flex max-w-7xl items-center justify-center border-t border-gray-600 bg-gray-800 p-2 lg:h-[50vh]">
-      <section className="font-poppins flex h-full w-full max-w-7xl flex-col items-center justify-center">
+      <section className=" flex h-full w-full max-w-7xl flex-col items-center justify-center text-sm text-gray-400">
         Pawel Kowalinski - 2024
       </section>
     </footer>
